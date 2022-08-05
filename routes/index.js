ea8f5662a5d747cf23fa8584176578e4628e8a28
unknown <leviettrung.social@gmail.com>
2022-08-05 23:38:09 +0700

    fix router *

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('*', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.json({
     status: 200,
     data: [new Date().toLocaleString("en-US", {
