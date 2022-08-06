b6f71ec5ad2922f024c1560f3818cbb7b7553975
unknown <leviettrung.social@gmail.com>
2022-08-06 10:57:12 +0700

    fix cfsbox update

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var rateLimit = require('express-rate-limit');
 var dotenv = require('dotenv');
-var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
@@ -13,36 +11,13 @@ dotenv.config();
 
 var app = express();
 
-corsConfig = {
-  origin: ['http://localhost:3000', "https://habacteam.web.app"],
-  credentials: true,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsConfig));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const apiLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 minutes
-  max: 2,
-  handler: function (req, res) {
-      res.status(429).send({
-          status: 500,
-          message: 'Too many requests!',
-      });
-  },
-  skip: (req, res) => {
-      if(req.ip === '::ffff:127.0.0.1')
-          return true;
-      return false;
-  }
-});
-
-app.use('/', apiLimiter, indexRouter);
-app.use('/api', apiLimiter, apiRouter);
+app.use('/', indexRouter);
+app.use('/api', apiRouter);
 
 module.exports = app;
--- routes/api.js
+++ routes/api.js
@@ -4,62 +4,80 @@ var axios = require('axios');
 var db = require('../config/firebase').db;
 require("firebase/firestore");
 
+
+
 /* GET users listing. */
 router.get('/hmc', async function (req, res, next) {
-  //lay du liệu từ facebook
-  const access_token = process.env.HMC_ACCESS_TOKEN;
-  const api_url = "https://graph.facebook.com/";
-  const page_id = "111037578336120";
-  const field = "about,fan_count,feed{message,permalink_url,full_picture,created_time,reactions.summary(total_count)}";
-  const full_url = api_url + page_id + "?fields=" + field + "&access_token=" + access_token;
-  const facebook_result = await axios.get(full_url)
-  const data = facebook_result.data;
-  //cap nhat du lieu vao firebase
-  await db.collection('facebook').doc('data').set(data);
-  //cap nhat thoi gian cap nhat
-  await db.collection('facebook').doc('update_time').set({
-    time: new Date().toLocaleString("en-US", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "numeric",
-      hour12: true,
-      timeZone: "Asia/Ho_Chi_Minh"
+  try {
+    //lay du liệu từ facebook
+    const access_token = process.env.HMC_ACCESS_TOKEN;
+    const api_url = "https://graph.facebook.com/";
+    const page_id = "111037578336120";
+    const field = "about,fan_count,feed{message,permalink_url,full_picture,created_time,reactions.summary(total_count)}";
+    const full_url = api_url + page_id + "?fields=" + field + "&access_token=" + access_token;
+    const facebook_result = await axios.get(full_url)
+    const data = facebook_result.data;
+    //cap nhat du lieu vao firebase
+    await db.collection('facebook').doc('data').set(data);
+    //cap nhat thoi gian cap nhat
+    await db.collection('facebook').doc('update_time').set({
+      time: new Date().toLocaleString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "numeric",
+        hour12: true,
+        timeZone: "Asia/Ho_Chi_Minh"
+      })
     })
-  })
-  res.json({
-    status: 200,
-    data: [new Date().toLocaleString("en-US", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "numeric",
-      hour12: true,
-      timeZone: "Asia/Ho_Chi_Minh"
-    })],
-  })
+    res.json({
+      status: 200,
+      message: "Facebook data updated successfully",
+      data: [new Date().toLocaleString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "numeric",
+        hour12: true,
+        timeZone: "Asia/Ho_Chi_Minh"
+      })],
+    })
+  } catch (error) {
+    res.json({
+      status: 500,
+      data: error
+    })
+  }
 });
 
-router.get('/hcfs',async function (req, res, next) {
-  const userRef = db.collection('cfs-box-users');
-  const snapshot = await userRef.get();
-  snapshot.forEach(async (doc) => {
-    await updateDoc(doc(db, "cfs-box-users", doc.id), {
-      cfs_per_day: 0,
-      status: true,
+router.get('/hcfs', async function (req, res, next) {
+  try {
+    const userRef = db.collection('cfs-box-users');
+    const snapshot = await userRef.get();
+    snapshot.forEach(async (doc) => {
+      await db.collection('cfs-box-users').doc(doc.id).update({
+        cfs_per_day: 0,
+        status: true,
+      })
     });
-  });
-  res.json({
-    status: 200,
-    data: [new Date().toLocaleString("en-US", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "numeric",
-      hour12: true,
-      timeZone: "Asia/Ho_Chi_Minh"
-    })],
-  })
+    res.json({
+      status: 200,
+      message: "Cfs box users updated",
+      data: [new Date().toLocaleString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "numeric",
+        hour12: true,
+        timeZone: "Asia/Ho_Chi_Minh"
+      })],
+    })
+  } catch (error) {
+    res.json({
+      status: 500,
+      data: error
+    })
+  }
 });
 
 router.get('*', function (req, res, next) {
