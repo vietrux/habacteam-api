5a4d52ca78a5a0633097a2989f04fcfc45b5659d
unknown <leviettrung.social@gmail.com>
2022-08-24 16:26:34 +0700

    add hr

--- routes/api.js
+++ routes/api.js
@@ -13,14 +13,58 @@ router.get('/hmc', async function (req, res, next) {
     const access_token = process.env.HMC_ACCESS_TOKEN;
     const api_url = "https://graph.facebook.com/";
     const page_id = "111037578336120";
-    const field = "about,fan_count,feed{message,permalink_url,full_picture,created_time,reactions.summary(total_count)}";
+    const field = "about,fan_count,feed{attachments{media,url},message,permalink_url,full_picture,created_time,reactions.summary(total_count)}";
     const full_url = api_url + page_id + "?fields=" + field + "&access_token=" + access_token;
     const facebook_result = await axios.get(full_url)
     const data = facebook_result.data;
     //cap nhat du lieu vao firebase
-    await db.collection('facebook').doc('data').set(data);
+    await db.collection('facebook').doc('hmc').set(data);
     //cap nhat thoi gian cap nhat
-    await db.collection('facebook').doc('update_time').set({
+    await db.collection('facebook').doc('update_time_hmc').set({
+      time: new Date().toLocaleString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "numeric",
+        hour12: true,
+        timeZone: "Asia/Ho_Chi_Minh"
+      })
+    })
+    res.json({
+      status: 200,
+      message: "Facebook data updated successfully",
+      data: [new Date().toLocaleString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "numeric",
+        hour12: true,
+        timeZone: "Asia/Ho_Chi_Minh"
+      })],
+    })
+  } catch (error) {
+    res.json({
+      status: 500,
+      data: error
+    })
+  }
+});
+
+/* GET users listing. */
+router.get('/hr', async function (req, res, next) {
+  try {
+    //lay du liệu từ facebook
+    const access_token = process.env.HR_ACCESS_TOKEN;
+    const api_url = "https://graph.facebook.com/";
+    const page_id = "283053355194941";
+    const field = "about,fan_count,feed{attachments{media,url},message,permalink_url,full_picture,created_time,reactions.summary(total_count)}";
+    const full_url = api_url + page_id + "?fields=" + field + "&access_token=" + access_token;
+    const facebook_result = await axios.get(full_url)
+    const data = facebook_result.data;
+    //cap nhat du lieu vao firebase
+    await db.collection('facebook').doc('hr').set(data);
+    //cap nhat thoi gian cap nhat
+    await db.collection('facebook').doc('update_time_hr').set({
       time: new Date().toLocaleString("en-US", {
         year: "numeric",
         month: "2-digit",
